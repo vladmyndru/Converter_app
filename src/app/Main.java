36fbe68436d303ca2acf4e804316290edcb1b40f
vladmyndru <ladvis62@gmail.com>
2024-10-23 15:09:37 +0300

    Add app description

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,7 +5,7 @@ public class Main {
     private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
-        System.out.println("Converter App.");
+        System.out.println("App for measures converting.");
 
         double kgs = 5;
         double pounds = convKgsToPounds(kgs);
